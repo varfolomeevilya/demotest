bba1d2cd55c30383af72fbbe6b93d1af9a851da5
илья <126683197+varfolomeevilya@users.noreply.github.com>
2023-11-01 10:33:22 +0300

    Update Car.java

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -3,88 +3,26 @@ package org.example;
 public class Car {
     private String name;
     private  Transmission transmission;
-        private Motor motor
-    }
-   
-
-      
+     private Motor motor
+    private  Bodywork bodywork
     
-    private  class Bodywork{
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        private String name;
-
-        public int getSize() {
-            return size;
-        }
-
-        public void setSize(int size) {
-            this.size = size;
-        }
-
-        private int size;
-    };
-    private  String Yearofrelease;
     private int power;
-    private class Wheels{
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        private String name;
-
-        public int getSize() {
-            return size;
-        }
-
-        public void setSize(int size) {
-            this.size = size;
-        }
-
-        private int size;
-
-        public int getFriction_force() {
-            return friction_force;
-        }
-
-        public void setFriction_force(int friction_force) {
-            this.friction_force = friction_force;
-        }
-
-        private int friction_force;
-    }
-
+    private Wheels wheels
 
     public int getPower() {
         return power;
-    }
-    public Car(String name , int power ){
-        this.name=name;
-        this.power=power;
-    }
-    public String getYearofmanufact() {
-
-        return Yearofrelease;
-    }
-
-    public void setYearofmanufact(String yearofmanufact) {
-        this.Yearofrelease=yearofmanufact;
-    }
+    } 
     public Car(){
         this.name=" formyla";
         this.power=123;
 
     }
+    public Car(String name , int power ){
+        this.name=name;
+        this.power=power;
+    }
+   
+   
     public void setPower(int power) {
         this.power = power;
     }
